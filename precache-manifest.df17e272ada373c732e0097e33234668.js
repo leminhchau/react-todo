62557717e0f34715eac3309a10ce82fc073ea1e3
Chau Le <cle6393@gmail.com>
2020-05-19 20:19:27 -0500

    Updates

---
+++ precache-manifest.df17e272ada373c732e0097e33234668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ad4a6dd47e93a91e67dc7e57bc314b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "0952b18be7d62708d72b",
+    "url": "/react-todo/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "160fd9769c7409e43afe",
+    "url": "/react-todo/static/js/2.e9d94c77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.e9d94c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0952b18be7d62708d72b",
+    "url": "/react-todo/static/js/main.31a01410.chunk.js"
+  },
+  {
+    "revision": "0b0c5f9c1cbe5b8041e6",
+    "url": "/react-todo/static/js/runtime-main.f4fd3597.js"
+  }
+]);
